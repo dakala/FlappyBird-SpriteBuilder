fbb8e561fce7fbf1ac22e189a7de55de3ab892f7
Deji Akala <dejiakala@gmail.com>
2015-01-22 19:45:11 +0000

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -22,8 +22,12 @@
     // Increment the time since the last obstacle was added
     timeSinceObstacle += delta; // delta is approximately 1/60th of a second
     
+    
+    NSLog(@"DELTA: %f", delta);
+    NSLog(@"TSO: %f", timeSinceObstacle);
+    
     // Check to see if two seconds have passed
-    if (timeSinceObstacle > 2.0f)
+    if (timeSinceObstacle < 2.0f)
     {
         // Add a new obstacle
         [self addObstacle];
